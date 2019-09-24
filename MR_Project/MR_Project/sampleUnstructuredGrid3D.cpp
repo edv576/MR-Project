69457bc68b25f693bf873f0453884790c98823d7
Floris Schild <Florisschild@hotmail.com>
2019-09-24 09:13:58 +0200

    .

--- MR_Project/MR_Project/sampleUnstructuredGrid3D.cpp
+++ MR_Project/MR_Project/sampleUnstructuredGrid3D.cpp
@@ -5,66 +5,70 @@
 #include "PlyReader.h"									//a reader that initializes UnstructuredGrid3D objects with meshes stored in the PLY file format
 #include "zpr.h"										//library for interactively manipulating the OpenGL camera (viewpoint) using the mouse
 
+#include <string>
+#include <filesystem>
+#include <fstream>
+namespace fs = std::filesystem;
 
-float fov	 = 60;										//Perspective projection parameters
-float z_near = -100; 
-float z_far  = 100;
+float fov = 60;										//Perspective projection parameters
+float z_near = -100;
+float z_far = 100;
 
 MeshRenderer		renderer;
-UnstructuredGrid3D*	grid = 0;
+UnstructuredGrid3D* grid = 0;
 int					drawing_style = 0;
 
 
 void viewing(int W, int H)								//Window resize function, sets up viewing parameters (GLUT callback function)
 {
 	glMatrixMode(GL_PROJECTION);						//1. Set the projection matrix
-	glLoadIdentity();	
-	gluPerspective(fov,float(W)/H,z_near,z_far);
+	glLoadIdentity();
+	gluPerspective(fov, float(W) / H, z_near, z_far);
 
-	glViewport(0,0,W,H);								//2. Set the viewport to the entire size of the rendering window
+	glViewport(0, 0, W, H);								//2. Set the viewport to the entire size of the rendering window
 }
 
 
 
 void mouseclick(int button, int state, int x, int y)	//Callback for mouse click events. We use these to control the viewpoint interactively.
 {
-  int keys = glutGetModifiers();						//The mouse events are interpreted as follows:	
-  if (keys & GLUT_ACTIVE_CTRL)							// 
-    button = GLUT_MIDDLE_BUTTON;						// -left button + move:                         rotate viewpoint
-  if (keys & GLUT_ACTIVE_SHIFT)							// -middle button (or left button+Control key): zoom in / out
-    button = GLUT_RIGHT_BUTTON;							// -right buttom (or left button+Shift key):    translate viewpoint
-  
-  zprMouse(button,state,x,y);							//Use the ZPR library to manipulate the viewpoint. The library sets the modelview  
-														//OpenGL matrix based on the mouse events, thereby changing the current viewpoint.
+	int keys = glutGetModifiers();						//The mouse events are interpreted as follows:	
+	if (keys & GLUT_ACTIVE_CTRL)							// 
+		button = GLUT_MIDDLE_BUTTON;						// -left button + move:                         rotate viewpoint
+	if (keys & GLUT_ACTIVE_SHIFT)							// -middle button (or left button+Control key): zoom in / out
+		button = GLUT_RIGHT_BUTTON;							// -right buttom (or left button+Shift key):    translate viewpoint
+
+	zprMouse(button, state, x, y);							//Use the ZPR library to manipulate the viewpoint. The library sets the modelview  
+														  //OpenGL matrix based on the mouse events, thereby changing the current viewpoint.
 }
 
 void mousemotion(int x, int y)							//Callback for mouse move events. We use these to control the viewpoint interactively.
 {
-  zprMotion(x,y);										//Pass the current location of the mouse to the ZPR library, to change the viewpoint.
+	zprMotion(x, y);										//Pass the current location of the mouse to the ZPR library, to change the viewpoint.
 
-  glutPostRedisplay();									//After each mouse move event, ask GLUT to redraw our window, so we see the viewpoint change.
+	glutPostRedisplay();									//After each mouse move event, ask GLUT to redraw our window, so we see the viewpoint change.
 }
 
-void keyboard(unsigned char c,int,int)					//Callback for keyboard events:
+void keyboard(unsigned char c, int, int)					//Callback for keyboard events:
 {
-	switch(c)	
+	switch (c)
 	{
 	case ' ':											// space:   Toggle through the various drawing styles of the mesh renderer
-		{
-		  drawing_style = (++drawing_style) % 4;		
-		  renderer.setDrawingStyle((MeshRenderer::DRAW_STYLE)drawing_style);
-		  break;
-		}
+	{
+		drawing_style = (++drawing_style) % 4;
+		renderer.setDrawingStyle((MeshRenderer::DRAW_STYLE)drawing_style);
+		break;
+	}
 	case 'R':											// 'r','R': Reset the viewpoint
 	case 'r':
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		zprInit(0,0,0);
+		zprInit(0, 0, 0);
 		break;
 	}
-	
-	glutPostRedisplay();			
-}	
+
+	glutPostRedisplay();
+}
 
 void draw()												//Render the 3D mesh (GLUT callback function)
 {
@@ -72,41 +76,85 @@ void draw()												//Render the 3D mesh (GLUT callback function)
 }
 
 
-int main(int argc,char* argv[])							//Main program
+int main(int argc, char* argv[])							//Main program
 {
-   cout<<"3D unstructured grid (mesh) visualization."<<endl;
-   cout<<"Interaction options: "<<endl<<endl;
-   cout<<"  Mouse: changes the viewpoint:"<<endl;
-   cout<<"      -rotate:     left-click and drag"<<endl;
-   cout<<"      -translate:  right-click (or Shift-left-click) and drag"<<endl;
-   cout<<"      -scale:      middle-click (or Control-left-click) and drag"<<endl;
-   cout<<"  Keyboard: visualization options:"<<endl;
-   cout<<"      -r,R:        reset the viewpoint"<<endl;
-   cout<<"      -space:      cycle through mesh rendering styles"<<endl;
+	if (argc > 2)
+	{
+		//enter statistics mode
+		string arg1 = argv[1];
+		if (arg1 == "-stat") {
 
-   const char* filename = (argc<2)? "DATA/bunny.ply" : argv[1];  //Read the PLY file given as 1st argument. If no arguments given, use a default file.
+			string arg2 = argv[2];
+			string dataFolder = "DATA";
 
-   glutInit(&argc, argv);								//1.  Initialize the GLUT toolkit
-   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-														//2.  Ask GLUT to create next windows with a RGB framebuffer and a Z-buffer too
-   glutInitWindowSize(500,500);							//3.  Tell GLUT how large are the windows we want to create next
-   glutCreateWindow("6. 3D mesh (unstructured grid)");	//4.  Create our window
-   zprInit(0,0,0);										//5.  Initialize the viewpoint interaction-tool to look at the point (0,0,0)
-   
-   PlyReader rdr;										//6.  Read a 3D mesh stored in a file in the PLY format
-   grid = rdr.read(filename);
-   grid->normalize();									//7.  Normalize the mesh in the [-1,1] cube. This makes setting the OpenGL projection easier.
-   grid->computeFaceNormals();							//8.  Compute face and vertex normals for the mesh. This allows us to shade the mesh next.
-   grid->computeVertexNormals();
-   
-   glutMouseFunc(mouseclick);							//9.  Bind the mouse click and mouse drag (click-and-move) events to callbacks. This allows us
-   glutMotionFunc(mousemotion);							//    next to control the viewpoint interactively.
-   glutKeyboardFunc(keyboard);
-   glutDisplayFunc(draw);								//10. Add a drawing callback to the window	
-   glutReshapeFunc(viewing);							//11. Add a resize callback to the window
-   glutMainLoop();										//12. Start the event loop that displays the graph and handles window-resize events
-   
-   return 0;
+			//use psb database
+			if (arg2 == "-psb")
+			{
+				for (const auto& dir : fs::directory_iterator(dataFolder + "/PSB"))
+				{
+					//get the dir as a category
+					std::string category = dir.path().stem().string();
+					std::ofstream myfile;
+
+					//list category
+					cout << category << endl << endl;
+
+					//loop through files
+					for (const auto& file : fs::directory_iterator(dir.path()))
+					{
+						cout << file.path().extension() << endl;
+
+						//if it is a mesh
+						if (file.path().extension().string() == ".off") {
+							//parse the file
+
+							//write the data to the csv
+
+							//profit!
+						}
+					}
+				}
+
+				return 0;
+			}
+		}
+	}
+
+
+
+	cout << "3D unstructured grid (mesh) visualization." << endl;
+	cout << "Interaction options: " << endl << endl;
+	cout << "  Mouse: changes the viewpoint:" << endl;
+	cout << "      -rotate:     left-click and drag" << endl;
+	cout << "      -translate:  right-click (or Shift-left-click) and drag" << endl;
+	cout << "      -scale:      middle-click (or Control-left-click) and drag" << endl;
+	cout << "  Keyboard: visualization options:" << endl;
+	cout << "      -r,R:        reset the viewpoint" << endl;
+	cout << "      -space:      cycle through mesh rendering styles" << endl;
+
+	const char* filename = (argc < 2) ? "DATA/bunny.ply" : argv[1];  //Read the PLY file given as 1st argument. If no arguments given, use a default file.
+
+	glutInit(&argc, argv);								//1.  Initialize the GLUT toolkit
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	//2.  Ask GLUT to create next windows with a RGB framebuffer and a Z-buffer too
+	glutInitWindowSize(500, 500);							//3.  Tell GLUT how large are the windows we want to create next
+	glutCreateWindow("6. 3D mesh (unstructured grid)");	//4.  Create our window
+	zprInit(0, 0, 0);										//5.  Initialize the viewpoint interaction-tool to look at the point (0,0,0)
+
+	PlyReader rdr;										//6.  Read a 3D mesh stored in a file in the PLY format
+	grid = rdr.read(filename);
+	grid->normalize();									//7.  Normalize the mesh in the [-1,1] cube. This makes setting the OpenGL projection easier.
+	grid->computeFaceNormals();							//8.  Compute face and vertex normals for the mesh. This allows us to shade the mesh next.
+	grid->computeVertexNormals();
+
+	glutMouseFunc(mouseclick);							//9.  Bind the mouse click and mouse drag (click-and-move) events to callbacks. This allows us
+	glutMotionFunc(mousemotion);							//    next to control the viewpoint interactively.
+	glutKeyboardFunc(keyboard);
+	glutDisplayFunc(draw);								//10. Add a drawing callback to the window	
+	glutReshapeFunc(viewing);							//11. Add a resize callback to the window
+	glutMainLoop();										//12. Start the event loop that displays the graph and handles window-resize events
+
+	return 0;
 }
 
 
